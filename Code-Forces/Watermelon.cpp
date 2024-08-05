521e468dba0766a88133d453eb67bdb9b03d23e2
Herry Patel <85663924+herry73@users.noreply.github.com>
2024-08-05 16:49:38 +0530

    rearrangement of files

---
+++ Code-Forces/Watermelon.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespaces std;
+
+int main()
+{
+  int w;
+  cout << "Enter The Weight: ";
+  cin >> w;
+  if (w%2 == 0)
+    {
+      cout << "YES" << endl;
+    }
+  else 
+    {
+      cout << "NO" << endl;
+    }
+}
